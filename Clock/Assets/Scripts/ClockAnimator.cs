23be395e69e4567947d030af4c9f1a21b610d165
Jason Marechal <jason@eugensystems.com>
2015-02-18 14:19:43 +0100

    Clock project

---
+++ Clock/Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform FHours, FMinutes, FSeconds;
+	public bool FAnalog = false;
+
+	private const float
+		HoursToDegrees = 360f / 12f,
+		MinutesToDegrees = 360f / 60f,
+		SecondsToDegrees = 360f / 60f;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FAnalog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			FHours.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalHours * -HoursToDegrees);
+			FMinutes.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalMinutes * -MinutesToDegrees);
+			FSeconds.localRotation =
+				Quaternion.Euler(0f,0f,(float)timespan.TotalSeconds * -SecondsToDegrees);
+		} else {
+			DateTime time = DateTime.Now;
+			FHours.localRotation = Quaternion.Euler (0.0f, 0.0f, time.Hour * -HoursToDegrees);
+			FMinutes.localRotation = Quaternion.Euler (0.0f, 0.0f, time.Minute * -MinutesToDegrees);
+			FSeconds.localRotation = Quaternion.Euler (0.0f, 0.0f, time.Second * -SecondsToDegrees);
+		}
+	}
+}
