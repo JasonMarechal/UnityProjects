4edc0cd30ddc2496ffc79a0b8c0fb3781b1c811d
Jason Marechal <jason@eugensystems.com>
2015-02-24 14:27:48 +0100

    Plus de couleur dans les fractal Ajout de lumière pour voir les couleur Forme aléatoire Les noeud rotationnent

--- Fractal/Assets/Scripts/Fractal.cs
+++ Fractal/Assets/Scripts/Fractal.cs
@@ -4,48 +4,86 @@ using System.Collections;
 
 
 public class Fractal : MonoBehaviour {
+    
+	public float FMaxRotationSpeed;
+	private float FRotationSpeed;
+    public int FMaxDepth;
+    public Material FMaterial;
+    public float FChildScale;
+    private int FDepth;
+	public Mesh[] FMeshs;
 
+    private static Vector3[] FChildDirections = 
+    {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
 
-	public int FMaxDepth;
-	public Mesh FMesh;
-	public Material FMaterial;
-	public float FChildScale;
-	private int FDepth;
+    private static Quaternion[] FChildOrientations =
+    {
+        Quaternion.identity,
+        Quaternion.Euler (0f, 0f, -90f),
+        Quaternion.Euler (0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f)
+    };
 
-	// Use this for initialization
-	void Start () {
-		gameObject.AddComponent<MeshFilter> ().mesh = FMesh;
-		gameObject.AddComponent<MeshRenderer>().material = FMaterial;
+    private Material[,] FMaterials;
+    private void InitializeMaterials () 
+    {
+        FMaterials = new Material[FMaxDepth + 1, 2];
+        for (int i = 0; i <= FMaxDepth; i++) {
+			float t = i / (FMaxDepth - 1f);
+			t *= t;
+            FMaterials[i, 0] = new Material(FMaterial);
+            FMaterials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+			FMaterials[i, 1] = new Material(FMaterial);
+			FMaterials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+		}
+		FMaterials[FMaxDepth, 0].color = Color.magenta;
+		FMaterials[FMaxDepth, 1].color = Color.red;
+    }
+
+    // Use this for initialization
+    void Start () {
+		FRotationSpeed = Random.Range(-FMaxRotationSpeed, FMaxRotationSpeed);
+        if (FMaterials == null) {
+            InitializeMaterials();
+        }
+		gameObject.AddComponent<MeshFilter> ().mesh = FMeshs[Random.Range(0, FMeshs.Length)];
+		gameObject.AddComponent<MeshRenderer> ().material = FMaterials [FDepth, Random.Range(0, 2)];
 		if (FDepth < FMaxDepth)
-			StartCoroutine (CreateChildren ());
-	}
+            StartCoroutine (CreateChildren ());
+    }
 
-	private void Initialize(Fractal parParent, Vector3 parDirection, Quaternion parOrientation)
-	{
-		FMesh = parParent.FMesh;
-		FMaterial = parParent.FMaterial;
-		FDepth = parParent.FDepth + 1;
-		FChildScale = parParent.FChildScale;
-		FMaxDepth = parParent.FMaxDepth;
+    private void Initialize(Fractal parParent, int parIndexChildren)
+    {
+		FMaxRotationSpeed = parParent.FMaxRotationSpeed;
+        FMeshs = parParent.FMeshs;
+        FMaterials = parParent.FMaterials;
+        FDepth = parParent.FDepth + 1;
+        FChildScale = parParent.FChildScale;
+        FMaxDepth = parParent.FMaxDepth;
 
-		transform.parent = parParent.transform;
-		transform.localScale = Vector3.one * FChildScale;
-		transform.localPosition = parDirection * (0.5f + 0.5f * FChildScale);
-		transform.localRotation = parOrientation;
-	}
+        transform.parent = parParent.transform;
+        transform.localScale = Vector3.one * FChildScale;
+        transform.localPosition = FChildDirections[parIndexChildren] * (0.5f + 0.5f * FChildScale);
+        transform.localRotation = FChildOrientations[parIndexChildren];
+    }
 
-	private IEnumerator CreateChildren()
-	{
-		yield return new WaitForSeconds(0.5f);
-		new GameObject ("Fractal Chaild").AddComponent<Fractal> ().Initialize(this, Vector3.up, Quaternion.identity);
-		yield return new WaitForSeconds(0.5f);
-		new GameObject ("Fractal Chaild").AddComponent<Fractal> ().Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-		yield return new WaitForSeconds(0.5f);
-		new GameObject ("Fractal Chaild").AddComponent<Fractal> ().Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    private IEnumerator CreateChildren()
+    {
+        for (int i = 0; i < FChildDirections.Length; ++i) {
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            new GameObject ("Fractal Chaild").AddComponent<Fractal> ().Initialize(this, i);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		transform.Rotate(0f, FRotationSpeed * Time.deltaTime, 0f);
 	}
 }
