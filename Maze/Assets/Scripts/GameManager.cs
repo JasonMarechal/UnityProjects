b8779bbfedd6278dd94b3dada4edae5b508fd772
Jason Marechal <jason@eugensystems.com>
2015-02-25 14:20:54 +0100

    Maze : Base objects Object Maze is a prefab

---
+++ Maze/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	private void Start () {
+		BeginGame();
+	}
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+	
+	private void BeginGame () {}
+	
+	private void RestartGame () {}
+}
